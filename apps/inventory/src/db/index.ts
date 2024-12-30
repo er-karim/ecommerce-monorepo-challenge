2a5a994ae84c5d37f23c49c214b2ea141dd5ccbf
er-karim <errakkarim@gmail.com>
2024-12-30 17:48:30 +0100

    feat(inventory): add dotenv support and configuration for environment variables

--- apps/inventory/src/db/index.ts
+++ apps/inventory/src/db/index.ts
@@ -2,8 +2,7 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL || "postgresql://user:pass@localhost:5432/mydb",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
--- apps/inventory/src/index.ts
+++ apps/inventory/src/index.ts
@@ -1,10 +1,12 @@
+import "dotenv/config";
+
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Inventory Microservice is running!");
+  res.send(`Inventory Microservice is running on port ${port}`);
 });
 
 app.listen(port, () => {
