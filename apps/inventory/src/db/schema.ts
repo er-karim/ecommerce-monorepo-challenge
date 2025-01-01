4a4b26d529ce124c1d837eea23cd786acd881ca0
er-karim <errakkarim@gmail.com>
2025-01-01 11:27:27 +0100

    refactor(inventory): remove files to prepare for refactoring

--- apps/inventory/src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, uuid, varchar, integer } from "drizzle-orm/pg-core";
-
-export const products = pgTable("products", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 255 }).notNull(),
-  inventoryCount: integer("inventory_count").notNull().default(0),
-});
